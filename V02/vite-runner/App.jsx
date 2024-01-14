348d4628f59099eed39de4bdd430ebe1f96bfb7b
kenyang <2663695289@qq.com>
2024-01-14 20:28:36 +0800

    day01: 支持jsx

---
+++ V02/vite-runner/App.jsx
@@ -0,0 +1,10 @@
+import React from './core/React.js'
+
+
+// const App = React.createElement('div', { id: 'app' }, 'hiiii')
+const App = <div>hi-mini-react</div>
+// function App() {
+//     return <div>hi-mini-react</div>
+// }
+
+export default App
